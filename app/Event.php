5fde6cf3761cb5bab3f6089d13083bab97427938
Vidur Butalia <vidurbutalia@gmail.com>
2019-01-26 15:19:32 +0530

    Added date format specifier to distinguish the YAML formatting from DB formatting.

--- app/Event.php
+++ app/Event.php
@@ -13,4 +13,9 @@ class Event extends Model
     protected $fillable = ['name', 'title', 'city', 'venue', 'start_time', 'end_time', 'url', 'blurb'];
     protected $dates = ['start_time', 'end_time'];
     protected $dateFormat = "Y-m-d H:i";
+
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s';
+    }
 }
